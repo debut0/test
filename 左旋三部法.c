e6841889a9aae8ecec2f691aa61f569b02dad672
debut0 <99167979+debut0@users.noreply.github.com>
2022-02-08 15:52:31 +0800

    Add files via upload

---
+++ 左旋三部法.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void left_move(char* arr,int k)  //Èý²½·¨×óÐý
+{
+      int sz=strlen(arr);
+      Reverse(arr,arr+sz-1);
+      Reverse(arr,arr+sz-k-1);
+      Reverse(arr+sz-k,arr+sz-1);
+}
+void Reverse(char* left,char* right)
+{
+    while(left<right)
+    {
+        char temp=*left;
+        *left=*right;
+        *right=temp;
+        left++;
+        right--;
+    }
+}
+int main()
+{
+    char arr[]="abcdef";
+    left_move(arr,2);
+
+    printf("%s\n",arr);
+    return 0;
+}
